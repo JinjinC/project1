246a9f5aa9563bbd56e94febf1c54fddec2a288d
wanzghen <wangzhen@blueocn.com.cn>
2015-03-17 10:34:17 +0800

    apm-price page table improvements

--- app/scripts/show-function.js
+++ app/scripts/show-function.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	//
 	$('.btn-knowmore').click(function  () {
-		var offset_top = $('.btn-knowmore').offset().top;
+		var offset_top = $('.btn-knowmore').offset().top-10;
 		$('body').animate({scrollTop:offset_top},1000);
 		setTimeout(function  () {
 			$('section.function').slideDown(1000);
@@ -10,13 +10,37 @@ $(document).ready(function() {
 	});
 
 	$(document).scroll(function () {
-		var scroll_top = $('body').scrollTop();
-		if (scroll_top >= 1400 ){
-			$('.function .table>thead>tr').addClass("tr-fixed");
-			$('.function .table>thead>tr>th').addClass("th-fixed");
-		} else{
-			$('.function .table>thead>tr').removeClass("tr-fixed");
-			$('.function .table>thead>tr>th').removeClass("th-fixed");
+
+		if ($('section.function').is(":hidden") == false) {
+
+			var scroll_top = $(document).scrollTop();
+			var th_offset_top = $('.function .table>thead>tr').offset().top;
+			var dis_to_window = th_offset_top-scroll_top;
+			var tr_offset_top = $('.function tbody tr:eq(0)').offset().top;
+			var dis_th_tr = tr_offset_top-th_offset_top;
+			var last_tr_offset_top = $('.function tbody tr:last').offset().top;
+			// console.log(dis_th_tr);
+			// console.log(th_offset_top);
+			// console.log(last_tr_offset_top);
+			// console.log(scroll_top);
+
+			if (dis_to_window <= 80 && th_offset_top <= last_tr_offset_top && scroll_top <= 3400 ){
+				$('.function .table>thead>tr').addClass("tr-fixed");
+				$('.function .table>thead>tr>th').addClass("th-fixed");
+				// $('.title-margin').addClass(".title-margin-b");
+
+			} else{
+				$('.function .table>thead>tr').removeClass("tr-fixed");
+				$('.function .table>thead>tr>th').removeClass("th-fixed");
+				// $('.title-margin').removeClass(".title-margin-b");
+			}
+
+			if (dis_th_tr > 38){
+				$('.function .table>thead>tr').removeClass("tr-fixed");
+				$('.function .table>thead>tr>th').removeClass("th-fixed");
+				// $('.title-margin').removeClass(".title-margin-b");
+			}
+
 		}
 	});
 	
