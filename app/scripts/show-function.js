3804c0471af161ca720d9c64d0de8652da2b5c27
wanzghen <wangzhen@blueocn.com.cn>
2015-03-17 19:40:28 +0800

    price  improvements

--- app/scripts/show-function.js
+++ app/scripts/show-function.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
 
 	//
 	$('.btn-knowmore').click(function  () {
-		var offset_top = $('.btn-knowmore').offset().top-10;
-		$('body').animate({scrollTop:offset_top},1000);
+		var offset_top = $('.btn-knowmore').offset().top+30;
+		$('body').animate({scrollTop:offset_top},600);
 		setTimeout(function  () {
-			$('section.function').slideDown(1000);
-		}, 1000);
+			$('section.function').slideDown(600);
+		}, 0);
 	});
 
 	$(document).scroll(function () {
