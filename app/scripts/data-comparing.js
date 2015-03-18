4481709cff6517b6471489ff8a2b06f55b962a43
wanzghen <wangzhen@blueocn.com.cn>
2015-03-18 16:51:40 +0800

    json file added and 992-1200......

---
+++ app/scripts/data-comparing.js
@@ -0,0 +1,39 @@
+$(document).ready(function  () {
+	
+	$.getJSON('../../data/apm-function.json', function (data) {
+
+		var html = '';
+		$.each(data, function(index, item){
+			html += '<tr>';
+			if (item.epr == ""){
+				html += '<td class="col1 col-subtitle">' + item.title + "</td>";
+				html += '<td></td>';
+				html += '<td></td>';
+				html += '<td></td>';
+			} else{
+				html += '<td class="col1">' + item.title + "</td>";
+				if (item.free == 1){
+					html += '<td>' + '<i class="fa fa-check"></i>' + '</td>';
+				}
+				else{
+					html += '<td></td>';
+				}
+				if (item.pro == 1){
+					html += '<td>' + '<i class="fa fa-check"></i>' + '</td>';
+				}
+				else{
+					html += '<td></td>';
+				}
+				if (item.epr == 1){
+					html += '<td>' + '<i class="fa fa-check"></i>' + '</td>';
+				}
+				else{
+					html += '<td></td>';
+				}
+			}
+			html += '</tr>';
+		});
+
+		$('.function tbody').html(html);
+	})
+});
--- app/scripts/show-function.js
+++ app/scripts/show-function.js
@@ -14,17 +14,20 @@ $(document).ready(function() {
 		if ($('section.function').is(":hidden") == false) {
 
 			var scroll_top = $(document).scrollTop();
+			var header_height = $('header').height();
 			var th_offset_top = $('.function .table>thead>tr').offset().top;
 			var dis_to_window = th_offset_top-scroll_top;
 			var tr_offset_top = $('.function tbody tr:eq(0)').offset().top;
 			var dis_th_tr = tr_offset_top-th_offset_top;
 			var last_tr_offset_top = $('.function tbody tr:last').offset().top;
+			var criteria_top = $('.function').offset().top;
+			var criteria_btm = $('.function tbody tr:nth-last-child(3)').offset().top;
 			// console.log(dis_th_tr);
 			// console.log(th_offset_top);
 			// console.log(last_tr_offset_top);
 			// console.log(scroll_top);
 
-			if (dis_to_window <= 80 && th_offset_top <= last_tr_offset_top && scroll_top <= 3400 ){
+			if (dis_to_window <= header_height && scroll_top  + header_height <= last_tr_offset_top && scroll_top < criteria_btm ){
 				$('.function .table>thead>tr').addClass("tr-fixed");
 				$('.function .table>thead>tr>th').addClass("th-fixed");
 				$('.table-title .btn').hide();
@@ -39,14 +42,9 @@ $(document).ready(function() {
 
 			if ($('.function .table>thead>tr').hasClass("tr-fixed")) {
 
-				if (dis_th_tr > 56) {
-					$('.table-title .btn').show();
-				};
-
-				if (dis_th_tr > 81){
+				if (dis_to_window>header_height || scroll_top+header_height>last_tr_offset_top || scroll_top>= criteria_btm || scroll_top<criteria_top){
 					$('.function .table>thead>tr').removeClass("tr-fixed");
 					$('.function .table>thead>tr>th').removeClass("th-fixed");
-					$('.table-title .btn').show();
 					// $('.title-margin').removeClass(".title-margin-b");
 				}
 			}
