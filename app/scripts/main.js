2359064499b17af781ad009701a9728e47fb5fb0
傅晓嵩 <fuxiaosong@oneapm.com>
2015-06-03 19:16:32 +0800

    把decode改为encode

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -272,7 +272,7 @@ $(document).ready(function() {
 
 ;(function(document){
 var fromValue = (function getURLParameter(name) {
-  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null
+  return encodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null
 })("from");
 if(fromValue){
   document.cookie = "from="+fromValue+";Path=/"+"; Domain=oneapm.com"
