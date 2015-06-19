d446869fad83a444dd234e7f4849818a7c986545
chenjinjin <chenjinjin@blueocn.com.cn>
2015-06-19 15:04:58 +0800

    企业版header

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -205,8 +205,10 @@ $(document).ready(function() {
         }
         if (mediaWidth < 768){
             $(".mi-headline-bg").css("height", $('.feature-mi').height() + 28*param*fmhParam +"px");
+            $(".ee-headline-bg").css("height", $('.feature-ee').height() + 50*param*fmhParam +"px");
         }else{
             $(".mi-headline-bg").removeAttr("style");
+            $(".ee-headline-bg").removeAttr("style");
         }
     }
 
