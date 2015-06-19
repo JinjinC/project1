c4335a2e56492ad647d9e5384ef74a27ab49d191
wanzghen <wangzhen@blueocn.com.cn>
2015-06-19 15:08:17 +0800

    price android ios page

--- app/scripts/sine-waves-action.js
+++ app/scripts/sine-waves-action.js
@@ -98,6 +98,70 @@ $(function(){
         gradient = void 0;
       }
     });
+    
+    var waves2 = new SineWaves({
+      el: document.getElementById('waves2'),
+
+      speed: 8,
+
+      width: function() {
+        return $('#waves2').parent().width()*1.4;
+      },
+
+      height: function() {
+        return $('#waves2').parent().height();
+      },
+
+      wavesWidth: '100%',
+
+      ease: 'SineInOut',
+
+      waves: [
+        {
+          timeModifier: 0.5 * params.wave_timeModifier,
+          lineWidth: 2,
+          amplitude: 120,
+          wavelength: 200,
+          segmentLength: 1
+        },
+        {
+          timeModifier: 0.5 * params.wave_timeModifier,
+          lineWidth: 2,
+          amplitude: 100,
+          wavelength: 150,
+          segmentLength: 1
+        },
+        {
+          timeModifier: 0.5 * params.wave_timeModifier,
+          lineWidth: 2,
+          amplitude: 50,
+          wavelength: 80,
+          segmentLength: 1
+        }
+      ],
+
+      initialize: function (){
+
+      },
+
+      resizeEvent: function() {
+        var gradient = this.ctx.createLinearGradient(0, 0, this.width, 0);
+        gradient.addColorStop(0,"rgba(255, 255, 255, 0)");
+        gradient.addColorStop(0.5,"rgba(255, 255, 255, 0.2)");
+        gradient.addColorStop(1,"rgba(255, 255, 255, 0)");
+
+        var index = -1;
+        var length = this.waves.length;
+          while(++index < length){
+          this.waves[index].strokeStyle = gradient;
+        }
+
+        // Clean Up
+        index = void 0;
+        length = void 0;
+        gradient = void 0;
+      }
+    });
 
     var sawtooth = new SineWaves({
       el: document.getElementById('sawtooth'),
@@ -152,67 +216,5 @@ $(function(){
       }
     });
 
-  var waves2 = new SineWaves({
-    el: document.getElementById('waves2'),
 
-    speed: 8,
-
-    width: function() {
-      return $('#waves2').parent().width()*1.4;
-    },
-
-    height: function() {
-      return $('#waves2').parent().height();
-    },
-
-    wavesWidth: '100%',
-
-    ease: 'SineInOut',
-
-    waves: [
-      {
-        timeModifier: 0.5 * params.wave_timeModifier,
-        lineWidth: 2,
-        amplitude: 120,
-        wavelength: 200,
-        segmentLength: 1
-      },
-      {
-        timeModifier: 0.5 * params.wave_timeModifier,
-        lineWidth: 2,
-        amplitude: 100,
-        wavelength: 150,
-        segmentLength: 1
-      },
-      {
-        timeModifier: 0.5 * params.wave_timeModifier,
-        lineWidth: 2,
-        amplitude: 50,
-        wavelength: 80,
-        segmentLength: 1
-      }
-    ],
-
-    initialize: function (){
-
-    },
-
-    resizeEvent: function() {
-      var gradient = this.ctx.createLinearGradient(0, 0, this.width, 0);
-      gradient.addColorStop(0,"rgba(255, 255, 255, 0)");
-      gradient.addColorStop(0.5,"rgba(255, 255, 255, 0.2)");
-      gradient.addColorStop(1,"rgba(255, 255, 255, 0)");
-
-      var index = -1;
-      var length = this.waves.length;
-        while(++index < length){
-        this.waves[index].strokeStyle = gradient;
-      }
-
-      // Clean Up
-      index = void 0;
-      length = void 0;
-      gradient = void 0;
-    }
-  });
 });
