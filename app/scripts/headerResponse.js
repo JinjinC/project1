2514ca13d385f5740b822c1afa40afb4f883dacb
wanzghen <wangzhen@blueocn.com.cn>
2015-07-13 16:37:28 +0800

    ai bi bi-price change

--- app/scripts/headerResponse.js
+++ app/scripts/headerResponse.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 	    var mediaWidth = $(document).width();
 	    var param =((768 - mediaWidth)/768 +1);
 	    var fmhParam = 1;
-	    if ($('.feature-mi').height<641) {
+	    if ($('.feature-mi').height()<641 || $('.feature-ai').height()<641 || $('.feature-bi').height()<641) {
 	        fmhPara = 0;
 	    }
 	    else{
@@ -12,9 +12,13 @@ $(document).ready(function(){
 	    }
 	    if (mediaWidth < 768){
 	        $(".mi-headline-bg").css("height", $('.feature-mi').height() + 28*param*fmhParam +"px");
+	        $(".ai-headline-bg").css("height", $('.feature-ai').height() + 28*param*fmhParam +"px");
+	        $(".bi-headline-bg").css("height", $('.feature-bi').height() + 28*param*fmhParam +"px");
 	        $(".ee-headline-bg").css("height", $('.feature-ee').height() + parseInt($('.feature-ee').css("padding-top")) + 20*param +"px");
 	    }else{
 	        $(".mi-headline-bg").removeAttr("style");
+	        $(".ai-headline-bg").removeAttr("style");
+	        $(".bi-headline-bg").removeAttr("style");
 	        $(".ee-headline-bg").removeAttr("style");
 	    }
 	}
