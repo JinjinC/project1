3946d2b6da8f62d300949da84f78091edb234f47
wanzghen <wangzhen@blueocn.com.cn>
2015-07-31 14:50:52 +0800

    bug fix

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,7 +7,23 @@ $(document).ready(function() {
     //global varible about videoId,beforescroll
     var global ={
         videoId: null,
-        beforescroll: 0
+        beforescroll: 0,
+        browser: function(){
+            var u = navigator.userAgent, app = navigator.appVersion;
+            return {
+                trident: u.indexOf("Trident") > -1, //IE内核
+                presto: u.indexOf("Presto") > -1, //opera内核
+                webKit: u.indexOf("AppleWebKit") > -1, //苹果、谷歌内核
+                gecko: u.indexOf("Gecko") > -1 && u.indexOf("KHTML") == -1, //火狐内核
+                mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+                ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+                android: u.indexOf("Android") > -1 || u.indexOf("Linux") > -1, //android终端或者uc浏览器
+                iPhone: u.indexOf("iPhone") > -1 , //是否为iPhone或者QQHD浏览器
+                iPad: u.indexOf("iPad") > -1, //是否iPad
+                webApp: u.indexOf("Safari") == -1, //是否web应该程序，没有头部与底部
+                mqq: u.indexOf("MQQ") >-1
+            };
+        }()
     }
     
     $(function () {
@@ -333,7 +349,7 @@ $(document).ready(function() {
         }, 500);
     })
 
-    $('.drop-down').on("click mouseenter",function(e){
+    $('.drop-down').on("click",function(e){
         var mediaWidth = $(window).width();
         if(mediaWidth<768){
             var $thisDropDown = $(this);
@@ -347,9 +363,27 @@ $(document).ready(function() {
             // $thisDropDown.find('.pro-content').toggle();
             $thisDropDown.toggleClass("open");
             e.preventDefault();
-        }
-        
+        }  
     })
+
+    if(!global.browser.mobile){
+        $('.drop-down').on("mouseenter",function(e){
+        var mediaWidth = $(window).width();
+        if(mediaWidth<768){
+            var $thisDropDown = $(this);
+            $('.drop-down').each(function(){
+                if ($(this).get(0) !== $thisDropDown.get(0)) {
+                    // $(this).find('.pro-content').removeAttr("style");
+                    // console.log($(this).get(0));
+                    $(this).removeClass("open");
+                }
+            })
+            // $thisDropDown.find('.pro-content').toggle();
+            $thisDropDown.toggleClass("open");
+            e.preventDefault();
+        }  
+    })
+    }
 });
 
 
