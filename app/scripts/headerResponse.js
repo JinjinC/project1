65d7dc3d04c76e16925684dd0f9cb0dc21ee2dc5
wanzghen <wangzhen@blueocn.com.cn>
2015-07-31 16:45:47 +0800

    height bug

--- app/scripts/headerResponse.js
+++ app/scripts/headerResponse.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 	function headerResize() {
-	    var mediaWidth = $(document).width();
+	    var mediaWidth = $(window).width();
 	    var param =((768 - mediaWidth)/768 +1);
 	    var fmhParam = 1;
 	    if ($('.feature-mi').height()<641 || $('.feature-ai').height()<641 || $('.feature-bi').height()<641) {
@@ -14,6 +14,7 @@ $(document).ready(function(){
 	        $(".mi-headline-bg").css("height", $('.feature-mi').height() + 28*param*fmhParam +"px");
 	        $(".ai-headline-bg").css("height", $('.feature-ai').height() + 28*param*fmhParam +"px");
 	        $(".bi-headline-bg").css("height", $('.feature-bi').height() + 28*param*fmhParam +"px");
+	        console.log('13');
 	        $(".ee-headline-bg").css("height", $('.feature-ee').height() + parseInt($('.feature-ee').css("padding-top")) + 20*param +"px");
 	    }else{
 	        $(".mi-headline-bg").removeAttr("style");
